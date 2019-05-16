4be4c148940c9aa7a19f4d04622fec63166cde5d
notmoebius <notmoebius@gmail.com>
2019-05-16 18:36:03 +0200

    update script codes la vie OK

--- script_6.js
+++ script_6.js
@@ -6,16 +6,6 @@ UUU ou UUC => Phénylalanine
 CGU ou CGC ou CGA ou CGG ou AGA ou AGG => Arginine
 UAU ou UAC => Tyrosine
 */
-const serine = ['UCU','UCC','UCA','UCG','AGU','AGC'];
-const proline = ['CCU','CCC','CCA','CCG'];
-const leucine = ['UUA','UUG'];
-const phenylalanine = ['UUU','UUC'];
-const arginine = ['CGU','CGC','CGA','CGG','AGA','AGG'];
-const tyrosine = ['UAU','UAC'];
-
-let arnToDecode1 = "UUACGCAGUAGA";
-let arnToDecode2 = "CCGUCGUUGCGCUACAGC";
-let arnToDecode3 = "CCUCGCCGGUACUUCUCG";
 
 function conversionProteine (codon) {
     switch (codon) {
@@ -30,7 +20,7 @@ function conversionProteine (codon) {
         case 'CCC':
         case 'CCA':
         case 'CCG':
-          return 'proline';
+          return 'Proline';
         case 'UUA':
         case 'UUG':
             return 'Leucine';
@@ -44,8 +34,8 @@ function conversionProteine (codon) {
         case 'AGA':
         case 'AGG':
             return 'Arginine';
-        case 'CCG':
-        case 'CCG':
+        case 'UAU':
+        case 'UAC':
             return 'Tyrosine';
         default:
           console.log("Cette entrée n'est pas reconnue");
@@ -56,22 +46,32 @@ function conversionProteine (codon) {
 function decomposeArn(arn) {
     let arrayArn = [];
     long = arn.length;
-    // while (condition) {
-        
-    // }
-    arrayArn.push(arn.slice(0, 3));
-    arrayArn.push(arn.slice(3, 6));
-    arrayArn.push(arn.slice(6, 9));
-    arrayArn.push(arn.slice(9, 12));
-    
+    let i = 0;
+    while (i<=long) {
+        arrayArn.push(arn.slice(i, i+3));
+        i = i + 3;
+    }
+    // arrayArn.push(arn.slice(0, 3));
+    // arrayArn.push(arn.slice(3, 6));
+    // arrayArn.push(arn.slice(6, 9));
+    // arrayArn.push(arn.slice(9, 12));
     return arrayArn;
 }
 
-let proteine = decomposeArn(arnToDecode1);
-let arrayProteine = [];
-for (let i = 0; i < proteine.length; i++) {
-    arrayProteine.push(conversionProteine(proteine[i])); 
+
+function afficheProteine(arnToDecode) {
+    let proteine = decomposeArn(arnToDecode);
+    let arrayProteine = [];
+    for (let i = 0; i < proteine.length-1; i++) {
+        arrayProteine.push(conversionProteine(proteine[i])); 
+    }
+    console.log(`ARN à décoder : ${arnToDecode} en ${arrayProteine.join('-')}`);
 }
-console.log(`ARN à décoder : ${arnToDecode1} en ${arrayProteine.join('-')}` )
-//console.log(arrayProteine.join('-'));
+
+// ARN à décoder = "UUACGCAGUAGA";
+// ARN à décoder  = "CCGUCGUUGCGCUACAGC";
+// ARN à décoder  = "CCUCGCCGGUACUUCUCG";
+afficheProteine("UUACGCAGUAGA");
+afficheProteine("CCGUCGUUGCGCUACAGC");
+afficheProteine("CCUCGCCGGUACUUCUCG");
 
